d88e5dbef8f3a98965a4db0be7e5a87c36a90e44
Sérgio Gabriel <sergio.gabriel85@yahoo.com>
2022-06-10 18:44:03 -0300

    primeiro commit

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            <header className="dashboard-header">
+            </header>
+        </div>
+    );
+}
+
+export default Dashboard;
