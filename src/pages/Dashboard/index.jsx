d182a336f6d6f6da01985ff005f1cdfdc14c646f
Sérgio Gabriel <sergio.gabriel85@yahoo.com>
2022-06-10 19:03:35 -0300

    incrementado header da dashboard e a parte do título do conteúdo

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,27 @@
 import './styles.css';
+import IconFilter from '../../assets/icon-filter.svg';
 
 function Dashboard() {
     return (
         <div className="dashboard">
+
             <header className="dashboard-header">
             </header>
+
+            <div className="content-dashboard">
+                <div className="title">
+                    <div className="left">
+                        <h1>Dashboard</h1>
+                        <span>Desafio Técnico Frontend</span>
+                    </div>
+                    <div className="right">
+                        <img src={IconFilter} alt="icon-filter" />
+                        <span>Filtrar</span>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 }
